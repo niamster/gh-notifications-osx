aaf875785e325ed30ca5d04d67c67e0072f00d87
Dmytro Milinevskyi <dmytro.milinevskyi@datadoghq.com>
2022-01-10 00:26:22 +0100

    Initial Commit

---
+++ gh-notifications-osx/gh_notifications_osxApp.swift
@@ -0,0 +1,17 @@
+//
+//  gh_notifications_osxApp.swift
+//  gh-notifications-osx
+//
+//  Created by Dmytro Milinevskyi on 10/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct gh_notifications_osxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
